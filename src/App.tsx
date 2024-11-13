72541b5fdabac2afa9fe63a082a1412a35b2ca9a
flynno <luke.laz.flynn@gmail.com>
2024-11-13 12:43:23 -0700

    added Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
 
 function App() {
+
+  const styles = {
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    }
+  }
   return (
-    <>
+    <div style={styles.root}>
       <Nav />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
