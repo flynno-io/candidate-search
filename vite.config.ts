eaeef0c282ea60b3a9ccb584b4146e29ce2f4858
flynno <luke.laz.flynn@gmail.com>
2024-11-14 22:20:28 -0700

    added comment to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
+  envDir: './environment', // Specify the directory where the environment files are located
   plugins: [react()],
 });
